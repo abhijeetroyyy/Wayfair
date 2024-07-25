d946b405b2bfade6256793774f2b27d011ea065d
Abhijeet Roy <98603767+abhijeetroyyy@users.noreply.github.com>
2024-07-25 14:52:03 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Headr from '@/components/Headr'
+import React from 'react';
+import Headr from '@/components/Headr';
 import "./globals.css";
 import Slider from '@/components/Slider';
 import Department from '@/components/Department';
@@ -9,20 +9,25 @@ import BrandCarousel from '@/components/BrandCarousel';
 import ImpactSection from '@/components/ImpactSection';
 import About from '@/components/About';
 import Footer from '@/components/Footer';
-const page = () => {
+import HorizontalScroll from '@/components/HorizontalScroll';
+
+const Page = () => {
   return (
     <>
-    <Headr/>
-    <Slider/>
-    <Department/>
-    <TopPicks/> 
-    <RugSale/>
-    <BrandCarousel/>
-    <ImpactSection/>
-    <About/>
-    <Footer/>
+      <Headr />
+      <Slider />
+      <Department />
+      <TopPicks />
+      <RugSale />
+      <div className="hidden sm-736:block">
+        <HorizontalScroll />
+      </div>
+      <BrandCarousel />
+      <ImpactSection />
+      <About />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default page
+export default Page;
