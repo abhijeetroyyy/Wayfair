3d08a0632b4892983d76a1157e15bea4a6a55ffb
dipjerry <kaxyapdip@gmail.com>
2024-07-25 21:45:59 +0530

    fix

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Headr from '@/components/Headr';
-import "./globals.css";
+// import "./globals.css";
 import Slider from '@/components/Slider';
 import Department from '@/components/Department';
 import TopPicks from '@/components/TopPicks';
